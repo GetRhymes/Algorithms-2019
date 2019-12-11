4d8f63555f59aaad23d008a9190ab9b6d01cae8a
GetRhymes <tarasenko.ns@edu.spbstu.ru>
2019-12-11 20:43:37 +0300

    6 les

--- src/lesson6/DynamicTasks.kt
+++ src/lesson6/DynamicTasks.kt
@@ -2,6 +2,11 @@
 
 package lesson6
 
+
+import java.io.*
+import java.lang.Math.min
+
+
 /**
  * Наибольшая общая подпоследовательность.
  * Средняя
@@ -15,9 +20,34 @@ package lesson6
  * При сравнении подстрок, регистр символов *имеет* значение.
  */
 fun longestCommonSubSequence(first: String, second: String): String {
-    TODO()
+    val firstLength = first.length
+    val secondLength = second.length
+    if (firstLength == 0 || secondLength == 0) return ""
+    val array = Array(first.length + 1) { IntArray(second.length + 1) }
+    for (i in 0 until firstLength) {
+        for (j in 0 until secondLength) {
+            if (first[i] == second[j]) {
+                array[i + 1][j + 1] = array[i][j] + 1
+            } else array[i + 1][j + 1] = maxOf(array[i][j + 1], array[i + 1][j])
+        }
+    }
+    val result = mutableListOf<Char>()
+    var indexFirst = firstLength
+    var indexSecond = secondLength
+    while (indexFirst != 0 && indexSecond != 0) {
+        when {
+            array[indexFirst][indexSecond] == array[indexFirst][indexSecond - 1] -> indexSecond--
+            array[indexFirst][indexSecond] == array[indexFirst - 1][indexSecond] -> indexFirst--
+            else -> {
+                result.add(0, first[indexFirst - 1])
+                indexFirst--
+                indexSecond--
+            }
+        }
+    }
+    return result.joinToString(separator = "")
 }
-
+// Time: O(mn), где m - длина первого слова, n - длина второго слова
 /**
  * Наибольшая возрастающая подпоследовательность
  * Сложная
@@ -55,8 +85,28 @@ fun longestIncreasingSubSequence(list: List<Int>): List<Int> {
  * Здесь ответ 2 + 3 + 4 + 1 + 2 = 12
  */
 fun shortestPathOnField(inputName: String): Int {
-    TODO()
+    val input = File(inputName).readLines()
+    val lines = input.size
+    val columns = input[0].split(" ").size
+    val table = Array(lines) { IntArray(columns) }
+    for (i in 0 until table.size) {
+        val lineInInput = input[i].split(" ")
+        for (j in 0 until table[i].size) table[i][j] = lineInInput[j].toInt()
+    }
+
+    for (i in 0 until lines)
+        for (j in 0 until columns)
+            when {
+                i > 0 && j > 0 -> table[i][j] += min(min(table[i - 1][j], table[i][j - 1]), table[i - 1][j - 1])
+                i > 0 -> table[i][j] += table[i - 1][j]
+                j > 0 -> table[i][j] += table[i][j - 1]
+            }
+
+    return table[lines - 1][columns - 1]
 }
+
+// Time: O(n*m), где m и n являются длинами прямоугольного поля (кол-во значений по длине и ширине)
+
 
 // Задачу "Максимальное независимое множество вершин в графе без циклов"
 // смотрите в уроке 5
